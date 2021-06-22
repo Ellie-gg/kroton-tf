364f8fa73e54d792f3e31f704f3baa6f5b4fb1f2
Ellie-gg <eliel.garcia@gmail.com>
2021-06-22 11:58:41 -0300

    commenting key on aws

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "kroton" {
   ami               = "ami-0ee02acd56a52998e"
   instance_type     = "t2.micro"
   security_groups   = ["${aws_security_group.kroton-ssh-http.name}"]
-  key_name = "parkroton"
+  #key_name = "parkroton"
   user_data = file("scripts/first-boot.sh")
   tags = {
         Name = "webserverkroton"
